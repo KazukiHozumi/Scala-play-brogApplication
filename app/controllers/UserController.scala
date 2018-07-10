14dc602cd12832d4bf48424a2403a973fa8e6c24
KazukiHozumi <hozumikazuki.8140@gmail.com>
2018-07-11 00:37:37 +0900

    fixed

---
+++ app/controllers/UserController.scala
@@ -0,0 +1,51 @@
+package controllers
+
+import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+import play.api.i18n.{MessagesApi, I18nSupport}
+import javax.inject.Inject
+import scalikejdbc._
+import models._
+
+class UserController @Inject()(val messagesApi: MessagesApi) extends Controller
+  with I18nSupport {
+
+  /**
+    * 一覧表示
+    */
+  def list = Action { implicit request =>
+    val u = Users.syntax("u")
+
+    DB.readOnly { implicit session =>
+      // ユーザのリストを取得
+      val users = withSQL {
+        select.from(Users as u).orderBy(u.id.asc)
+      }.map(Users(u.resultName)).list.apply()
+
+      // 一覧画面を表示
+      Ok(views.html.user.list(users))
+    }
+  }
+
+  /**
+    * 編集画面表示
+    */
+  def edit(id: Option[Long]) = TODO
+
+  /**
+    * 登録実行
+    */
+  def create = TODO
+
+  /**
+    * 更新実行
+    */
+  def update = TODO
+
+  /**
+    * 削除実行
+    */
+  def remove(id: Long) = TODO
+
+}
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,2 +1,5 @@
 // The Play plugin
 addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.5.16")
+
+libraryDependencies += "com.h2database" % "h2" % "1.4.193"
+addSbtPlugin("org.scalikejdbc" %% "scalikejdbc-mapper-generator" % "2.5.1")
