c4667f30e7d61202ab48bfdd4d3869e1c7e271c5
KazukiHozumi <hozumikazuki.8140@gmail.com>
2018-07-11 18:44:14 +0900

    finish

--- app/controllers/UserController.scala
+++ app/controllers/UserController.scala
@@ -8,8 +8,24 @@ import javax.inject.Inject
 import scalikejdbc._
 import models._
 
+object UserController {
+  // フォームの値を格納するケースクラス
+  case class UserForm(id: Option[Long], name: String, companyId: Option[Int])
+
+  // formから送信されたデータ ⇔ ケースクラスの変換を行う
+  val userForm = Form(
+    mapping(
+      "id"        -> optional(longNumber),
+      "name"      -> nonEmptyText(maxLength = 20),
+      "companyId" -> optional(number)
+    )(UserForm.apply)(UserForm.unapply)
+  )
+}
+
 class UserController @Inject()(val messagesApi: MessagesApi) extends Controller
   with I18nSupport {
+
+
 
   /**
     * 一覧表示
@@ -28,24 +44,93 @@ class UserController @Inject()(val messagesApi: MessagesApi) extends Controller
     }
   }
 
+  import UserController._
+
   /**
     * 編集画面表示
     */
-  def edit(id: Option[Long]) = TODO
+  def edit(id: Option[Long]) = Action { implicit request =>
+    val c = Companies.syntax("c")
+
+    DB.readOnly { implicit session =>
+      // リクエストパラメータにIDが存在する場合
+      val form = id match {
+        // IDが渡されなかった場合は新規登録フォーム
+        case None => userForm
+        // IDからユーザ情報を1件取得してフォームに詰める
+        case Some(id) => {
+          val user = Users.find(id).get
+          userForm.fill(UserForm(Some(user.id), user.name, user.companyId))
+        }
+      }
+
+      // プルダウンに表示する会社のリストを取得
+      val companies = withSQL {
+        select.from(Companies as c).orderBy(c.id.asc)
+      }.map(Companies(c.resultName)).list().apply()
+
+      Ok(views.html.user.edit(form, companies))
+    }
+  }
 
   /**
     * 登録実行
     */
-  def create = TODO
+  def create = Action { implicit request =>
+    DB.localTx { implicit session =>
+      // リクエストの内容をバインド
+      userForm.bindFromRequest.fold(
+        // エラーの場合
+        error => {
+          BadRequest(views.html.user.edit(error, Companies.findAll()))
+        },
+        // OKの場合
+        form  => {
+          // ユーザを登録
+          Users.create(form.name, form.companyId)
+          // 一覧画面へリダイレクト
+          Redirect(routes.UserController.list)
+        }
+      )
+    }
+  }
 
   /**
     * 更新実行
     */
-  def update = TODO
+  def update = Action { implicit request =>
+    DB.localTx { implicit session =>
+      // リクエストの内容をバインド
+      userForm.bindFromRequest.fold(
+        // エラーの場合は登録画面に戻す
+        error => {
+          BadRequest(views.html.user.edit(error, Companies.findAll()))
+        },
+        // OKの場合は登録を行い一覧画面にリダイレクトする
+        form => {
+          // ユーザ情報を更新
+          Users.find(form.id.get).foreach { user =>
+            Users.save(user.copy(name = form.name, companyId = form.companyId))
+          }
+          // 一覧画面にリダイレクト
+          Redirect(routes.UserController.list)
+        }
+      )
+    }
+  }
 
   /**
     * 削除実行
     */
-  def remove(id: Long) = TODO
+  def remove(id: Long) = Action { implicit request =>
+    DB.localTx { implicit session =>
+      // ユーザを削除
+      Users.find(id).foreach { user =>
+        Users.destroy(user)
+      }
+      // 一覧画面へリダイレクト
+      Redirect(routes.UserController.list)
+    }
+  }
 
 }
